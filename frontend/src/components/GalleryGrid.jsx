796aaa7aa13626daf45b2d3ec7316a4513398a51
greenpower90 <144276799+greenpower90@users.noreply.github.com>
2024-09-06 18:03:59 +0200

    gallery modal next and prevous function implementation

--- frontend/src/components/GalleryGrid.jsx
+++ frontend/src/components/GalleryGrid.jsx
@@ -5,15 +5,29 @@ function GalleryGrid(props) {
     const pictureDataAll = props.Src
 
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [index, setIndex] = useState(null)
     const [selectedImage, setSelectedImage] = useState(null);
 
-    function openModal(selectedImage){
+    function openModal(selectedImage, index){
         setSelectedImage(selectedImage)
         setIsModalOpen(true)
+        setIndex(index)
     }
 
     function closeModal(){
         setIsModalOpen(false)
+    }
+
+    function nextPicture(index){
+      const newIndex = index + 1 < pictureDataAll.length ? index + 1 : 0
+      setIndex(newIndex)
+      setSelectedImage(pictureDataAll[newIndex])
+    }
+
+    function previousPicture(index){
+      const newIndex = index - 1 < 0 ? pictureDataAll.length -1 : index - 1
+      setIndex(newIndex)
+      setSelectedImage(pictureDataAll[newIndex])
     }
 
     return (<div>
@@ -25,17 +39,20 @@ function GalleryGrid(props) {
                             style={{ width: "100%", height: "100%", objectFit: "cover" }}
                             className={`gallery-img ${pictureData.dimension.spanCollumn} ${pictureData.dimension.spanRow} `}
                             onClick={()=>{
-                                openModal(pictureData)
+                                openModal(pictureData, index)
                             }} 
                             />
             })}
         </div>
 
         {isModalOpen && (
-        <div className="modal" onClick={closeModal}>
+        <div className="modal" >
+          <div onClick={()=>{previousPicture(index)}} style={{color: "white"}}>PREVIOUS</div>
           <div className="modal-content">
-            <img src={'http://localhost:5000'+selectedImage.fileLocation.full} alt="Full size" style={{ maxWidth: '100%', height: '100%' }} />
+            <img onClick={closeModal} src={'http://localhost:5000'+selectedImage.fileLocation.full} alt="Full size" style={{ maxWidth: '100%', height: '100%' }} />
+            
           </div>
+          <div onClick={()=>{nextPicture(index)}} style={{color: "white"}}>NEXT</div>
         </div>
       )}
 
